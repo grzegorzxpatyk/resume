b6d758abf377cc553470543dacf6ebdb7b819996
Grzegorz Patyk <grzegorzxpatyk@gmail.com>
2023-09-01 02:00:33 +0200

    extend tailwind's config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,15 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-blue-gradient': 'rgba(10, 18, 47, 0.8)',
+        'dark-gray-gradient': 'rgba(0, 0, 0, 0.8)'
+      },
+      backgroundImage: {
+        'noise': "url(\"data:image/svg+xml,%3Csvg viewBox='0 0 250 250' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='3' numOctaves='3' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%' height='100%' filter='url(%23noiseFilter)'/%3E%3C/svg%3E\")"
+      }
+    },
   },
   plugins: [],
 }
-
