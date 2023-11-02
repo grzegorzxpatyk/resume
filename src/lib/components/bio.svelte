618d5b74761f01a9f1e434f5180707ad603f0cad
Grzegorz Patyk <grzegorzxpatyk@gmail.com>
2023-11-02 13:33:50 +0100

    use flip animation on profile pic

--- src/lib/components/bio.svelte
+++ src/lib/components/bio.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <section class="flex flex-col justify-start items-start pb-6 border-b border-b-zinc-800 dark:border-b-zinc-400">
-	<img src={profile} alt={picDescription} class="rounded-full mb-8 min-w-[120px] w-5/6 self-center sm:self-auto sm:w-full shadow-2xl shadow-zinc-500 dark:shadow-zinc-700 print:shadow-none" />
+	<img src={profile} alt={picDescription} class="rounded-full mb-8 min-w-[120px] w-5/6 self-center sm:self-auto sm:w-full shadow-2xl shadow-zinc-500 dark:shadow-zinc-700 print:shadow-none hover:animate-flip" />
 	<h1 class="text-zinc-950 dark:text-zinc-100 text-3xl mb-4 text-left font-extrabold">{name}</h1>
 	<h2 class="dark:text-zinc-300 text-xl font-medium">{jobTitle}</h2>
 </section>
